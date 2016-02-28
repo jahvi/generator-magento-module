70c7aecf49a1be75cfc09190379cdf8042f64ecc
Javier Villanueva <javiervd@gmail.com>
2016-02-28 16:12:21 +0000

    Add default index action to generated controller

--- generators/app/templates/controller.php
+++ generators/app/templates/controller.php
@@ -5,7 +5,16 @@
  * @category   <%= namespace %>
  * @package    <%= moduleName %>
  */
-class %= moduleName %>_IndexController extends Mage_Core_Controller_Front_Action
+class <%= moduleName %>_IndexController extends Mage_Core_Controller_Front_Action
 {
-    //
+    /**
+     * indexAction description
+     *
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
 }
